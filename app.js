009df852b13efd1c892a629d063a528bcf0d9965
Cobraaz <anujbansal077@gmail.com>
2020-07-24 15:10:18 +0530

    Added user login and signup in server side

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5000;
+const { MONGOURI } = require('./Keys');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+//MongoDB Schema
+require('./models/user');
+
+//require Router file
+app.use(express.json());
+app.use(require('./routes/auth'));
+
+//MongoDb connection
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('error in  connecting', err);
 });
 
 app.listen(PORT, () => {
