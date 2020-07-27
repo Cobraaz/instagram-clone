3fbbf8fb795a58bcb1ed76e22811f12599bfa022
Cobraaz <anujbansal077@gmail.com>
2020-07-27 14:16:28 +0530

    Added Reset password and searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Signin from "./components/screens/SignIn";
 import CreatePost from "./components/screens/CreatePost";
 import UserProfile from "./components/screens/UserProfile";
 import SubscribedUserPosts from "./components/screens/SubscribesUserPosts";
+import Reset from "./components/screens/Reset";
+import NewPassword from "./components/screens/Newpassword";
 
 import { reducer, initialState } from "./reducers/userReducer";
 export const UserContext = createContext();
@@ -21,9 +23,11 @@ const Routing = () => {
     if (user) {
       dispatch({ type: "USER", payload: user });
     } else {
-      history.push("/signin");
+      if (!history.location.pathname.startsWith("/reset"))
+        history.push("/signin");
     }
   }, []);
+
   return (
     <Switch>
       <Route exact path="/">
@@ -46,6 +50,12 @@ const Routing = () => {
       </Route>
       <Route path="/myfollowingpost">
         <SubscribedUserPosts />
+      </Route>
+      <Route exact path="/reset">
+        <Reset />
+      </Route>
+      <Route path="/reset/:token">
+        <NewPassword />
       </Route>
     </Switch>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../App";
-const Navbar = () => {
-  const history = useHistory();
+import M from "materialize-css";
+const NavBar = () => {
+  const searchModal = useRef(null);
+  const [search, setSearch] = useState("");
+  const [userDetails, setUserDetails] = useState([]);
   const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+  }, []);
   const renderList = () => {
     if (state) {
       return [
-        <li>
+        <li key="1">
+          <i
+            data-target="modal1"
+            className="large material-icons modal-trigger"
+            style={{ color: "black" }}
+          >
+            search
+          </i>
+        </li>,
+        <li key="2">
           <Link to="/profile">Profile</Link>
         </li>,
-        <li>
+        <li key="3">
           <Link to="/create">Create Post</Link>
         </li>,
         <li key="4">
           <Link to="/myfollowingpost">My following Posts</Link>
         </li>,
-        <li>
+        <li key="5">
           <button
-            className="btn waves-effect waves-light #4fc3f7 light-blue lighten-2"
+            className="btn #c62828 red darken-3"
             onClick={() => {
               localStorage.clear();
               dispatch({ type: "CLEAR" });
@@ -31,14 +47,31 @@ const Navbar = () => {
       ];
     } else {
       return [
-        <li>
-          <Link to="/signin">SignIn</Link>
+        <li key="6">
+          <Link to="/signin">Signin</Link>
         </li>,
-        <li>
-          <Link to="signup">SignUp</Link>
+        <li key="7">
+          <Link to="/signup">Signup</Link>
         </li>,
       ];
     }
+  };
+
+  const fetchUsers = (query) => {
+    setSearch(query);
+    fetch("/search-users", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    })
+      .then((res) => res.json())
+      .then((results) => {
+        setUserDetails(results.user);
+      });
   };
   return (
     <nav>
@@ -50,7 +83,48 @@ const Navbar = () => {
           {renderList()}
         </ul>
       </div>
+      <div
+        id="modal1"
+        class="modal"
+        ref={searchModal}
+        style={{ color: "black" }}
+      >
+        <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e) => fetchUsers(e.target.value)}
+          />
+          <ul className="collection">
+            {userDetails.map((item) => {
+              return (
+                <Link
+                  to={
+                    item._id !== state._id ? "/profile/" + item._id : "/profile"
+                  }
+                  onClick={() => {
+                    M.Modal.getInstance(searchModal.current).close();
+                    setSearch("");
+                  }}
+                >
+                  <li className="collection-item">{item.email}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <button
+            className="modal-close waves-effect waves-green btn-flat"
+            onClick={() => setSearch("")}
+          >
+            close
+          </button>
+        </div>
+      </div>
     </nav>
   );
 };
-export default Navbar;
+
+export default NavBar;
